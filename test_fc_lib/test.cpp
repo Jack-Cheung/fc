1e97434848ccca0838875a3cc77fc216482e54f5
Jack-Cheung <374511110@qq.com>
2019-03-08 13:45:22 +0800

    r1 key test ok

--- test_fc_lib/test.cpp
+++ test_fc_lib/test.cpp
@@ -7,13 +7,44 @@
 using namespace fc::crypto;
 using namespace fc;
 
+#define CHECK_EQUAL( A, B ) \
+ std::cout<<(A)<<std::endl;\
+ std::cout<<(B)<<std::endl;\
+ if((A) != (B))\
+ {\
+	 std::cout << A << " != " << B << std::endl;\
+ }
+
 int main()
 {
-	auto private_key_string = std::string("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3");
-	auto expected_public_key = std::string("EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV");
-	auto test_private_key = private_key(private_key_string);
-	auto test_public_key = test_private_key.get_public_key();
 
-	std::cout << std::string(test_private_key) << std::endl;
-	std::cout << std::string(test_public_key) << std::endl;;
+	do{
+		auto private_key_string = std::string("PVT_R1_iyQmnyPEGvFd8uffnk152WC2WryBjgTrg22fXQryuGL9mU6qW");
+		auto expected_public_key = std::string("PUB_R1_6EPHFSKVYHBjQgxVGQPrwCxTg7BbZ69H9i4gztN9deKTEXYne4");
+		auto test_private_key = private_key(private_key_string);
+		auto test_public_key = test_private_key.get_public_key();
+		CHECK_EQUAL(private_key_string, std::string(test_private_key));
+		CHECK_EQUAL(expected_public_key, std::string(test_public_key));
+	} while(false);
+
+	do{
+		auto payload = "Test Cases";
+		auto digest = sha256::hash(payload, const_strlen(payload));
+		auto key = private_key::generate<r1::private_key_shim>();
+		auto pub = key.get_public_key();
+		auto sig = key.sign(digest);
+		auto recovered_pub = public_key(sig, digest);
+		std::cout << recovered_pub << std::endl;
+		CHECK_EQUAL(std::string(recovered_pub), std::string(pub));
+	} while(false);
+
+	do{
+		auto key = private_key::generate<r1::private_key_shim>();
+		auto pub = key.get_public_key();
+		auto pub_str = std::string(pub);
+		auto recycled_pub = public_key(pub_str);
+		std::cout << pub << " -> " << recycled_pub << std::endl;
+		CHECK_EQUAL(std::string(pub), std::string(recycled_pub));
+	} while(false);
+
 }
